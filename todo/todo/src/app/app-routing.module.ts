b90c246bd5f790f16e54440f2af6e50cef98862a
Cristian <thark.cris@gmail.com>
2019-06-24 10:43:11 +0200

    segundo Commit-hasta la review

---
+++ todo/todo/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { ListTodosComponent } from './list-todos/list-todos.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+
+// welcome 
+const routes: Routes = [
+  { path: '', component: LoginComponent,  },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome/:name', component: WelcomeComponent },
+  { path: 'todos', component: ListTodosComponent },
+  { path: '**', component: ErrorComponent }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
